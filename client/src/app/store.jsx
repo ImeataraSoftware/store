b819249fe9481c24cc73e55c79c571b7f555fbad
sebacolladott <105157508+sebacolladott@users.noreply.github.com>
2023-04-19 16:41:56 -0300

    Seba

--- client/src/app/store.jsx
+++ client/src/app/store.jsx
@@ -3,9 +3,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 
 export const store = configureStore({
-  reducer: {
-    [pokemonApi.reducerPath]: pokemonApi.reducer,
-  },
+  reducer: {},
 
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(),
 });
